22a5a76295bfe48eaabca88d948b593e005a6762
Tim Quimbo <tquimbo1@gmail.com>
2021-09-21 23:43:54 -0400

    App controller, views, models update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
+require './config/environment'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, ENV["SESSION_SECRET"]
+    register Sinatra::Flash
+  end
+
+  get "/" do
+    erb :welcome
+  end
+
 end
